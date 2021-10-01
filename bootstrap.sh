74a37b8fc74d4bc58cda5449d2844ebb6c07cdaf
Tiest Vilee <tiestvilee@users.noreply.github.com>
2021-10-01 12:22:22 +0100

    logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,15 +10,25 @@ rm -rf master.zip
 ./dotfiles-master/brew/setup.sh
 rm -rf dotfiles-master
 
+echo "----- clone dotfiles ----- "
 git clone https://github.com/tiestvilee/dotfiles.git .dotfiles
 pushd .dotfiles
 git remote rm origin
 git remote add origin git@github.com:tiestvilee/dotfiles.git
 popd
 
+echo "----- git ----- "
 .dotfiles/git/setup.sh
+
+# echo "----- gpg ----- "
 #.dotfiles/gpg/setup.sh
+
+echo "----- osx ----- "
 .dotfiles/osx/setup.sh
+
+echo "----- zsh ----- "
 .dotfiles/zsh/setup.sh
+
+echo "----- dev ----- "
 .dotfiles/dev/setup.sh
 
